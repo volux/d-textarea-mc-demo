540bd9681ca3b55e04cf55969a4b348af5625250
volux <andrey.skulov@gmail.com>
2015-11-10 05:23:03 +0300

    Add example server

--- server.js
+++ server.js
@@ -1,15 +1,8 @@
-//Docker related configuration, can disregard if not using Docker
-if (process.env.MONGO_PORT_27017_TCP_ADDR) {
-  process.env.MONGO_HOST = process.env.MONGO_PORT_27017_TCP_ADDR;
-}
-if (process.env.MONGO_PORT_27017_TCP_PORT) {
-  process.env.MONGO_PORT = process.env.MONGO_PORT_27017_TCP_PORT;
-}
-if (process.env.REDIS_PORT_6379_TCP_ADDR != void 0 && process.env.REDIS_PORT_6379_TCP_PORT != void 0) {
-  process.env.REDIS_HOST = process.env.REDIS_PORT_6379_TCP_ADDR;
-  process.env.REDIS_PORT = process.env.REDIS_PORT_6379_TCP_PORT;
-}
-process.env.MONGO_DB = "d-textarea-mc-demo";
-process.env.NODE_ENV = "production";
+//process.env.NODE_ENV = 'production';
 
-require('./textarea/server');
+var options = require('./config');
+var apps = [
+  require('./apps/textarea')
+];
+
+require('derby-example-server')(options, apps);
